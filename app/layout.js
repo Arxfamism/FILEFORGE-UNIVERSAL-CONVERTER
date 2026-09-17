4e83a307d298f9a406465841a5654f164373fef6
ARSALAN QURESHI <sahilalee21@gmail.com>
2026-09-17 16:51:08 +0500

    Delete app directory

--- app/layout.js
+++
@@ -1,3 +0,0 @@
-import './globals.css';
-export const metadata={title:'FileForge Universal Converter',description:'Privacy-first browser file converter'};
-export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
