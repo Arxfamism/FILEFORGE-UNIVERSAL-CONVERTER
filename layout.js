3ab85a95be7d049a0dc16cb499d0b61f89ccd661
ARSALAN QURESHI <sahilalee21@gmail.com>
2026-09-17 16:16:16 +0500

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,3 @@
+import './globals.css';
+export const metadata={title:'FileForge Universal Converter',description:'Privacy-first browser file converter'};
+export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
